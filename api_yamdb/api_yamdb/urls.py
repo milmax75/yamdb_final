55709d1ea79e975b0b3d6d3c7dfcbf115d44a752
milmax75 <milmax@list.ru>
2023-03-19 19:07:43 +0200

    isort 1.3

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,8 +1,8 @@
+from api.views import APISignUp, SendToken
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
-from api.views import APISignUp, SendToken
 
 urlpatterns = [
     path('admin/', admin.site.urls),
