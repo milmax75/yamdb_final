75a3b978a47a88268edad8015d750af3492374ac
milmax75 <milmax@list.ru>
2023-03-20 18:27:48 +0200

    secrets added

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -66,8 +66,8 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'USER': os.getenv('POSTGRES_USER', default='${{ secrets.DB_USER }}'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='${{ secrets.DB_PSW }}'),
         'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default='5432')
     }
